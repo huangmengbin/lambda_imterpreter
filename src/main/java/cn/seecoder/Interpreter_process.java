8b1a59b0fde1c26a2fbeb8fd86a0e4a3b60592a9
HMB <1352482583@qq.com>
2019-06-16 10:14:39 +0800

    67nb

--- src/main/java/cn/seecoder/Interpreter_process.java
+++
@@ -1,9 +0,0 @@
-package cn.seecoder;
-
-class Interpreter_process extends Interpreter_getline{
-    Interpreter_process(Parser p,int print_mode){
-        myast = p.parse();
-        step=999999999;
-        this.print_mode=print_mode;
-    }
-}
